f88dec2ca50151c96e3f443ad31fe7770616fba5
LuckyLion <always.smile0915@gmail.com>
2017-04-20 22:09:20 -0400

    updated the code for extracting flash data

--- ScrapingFlash/spiders/ScrapingFlash.py
+++ ScrapingFlash/spiders/ScrapingFlash.py
@@ -6,52 +6,30 @@ from xml.sax.saxutils import unescape
 
 class FlashApp(scrapy.Spider):
     name = "flash"
+
     allowed_domains = ['https://hooktheory.com']
-    start_urls = ['https://www.hooktheory.com/theorytab/difficulties']
+    start_urls = ['https://www.hooktheory.com/theorytab/difficulties/intermediate?page=50']
+
+    API_URL = 'https://www.hooktheory.com/songs/getXmlByPk?pk={id}'
 
     def start_requests(self):
         yield scrapy.Request(self.start_urls[0], callback=self.parse)
 
     def parse(self, response):
-        #yield scrapy.Request(url='https://www.hooktheory.com/theorytab/view/zun/fall-of-fall---autumnal-waterfall', callback=self._parse_data, dont_filter=True)
-        href_links = response.xpath('//div[@class="row"]/div[contains(@class,"col-xs-4 text-right")]'
-                                     '/h1[@class="tight"]/a/@href').extract()
+        href_links = response.xpath('//ul[@class="grid2468"]/li[@class="grid-item"]/a/@href').extract()
 
         for href in href_links:
             url = 'https://www.hooktheory.com%s' % href
+            yield scrapy.Request(url=url, callback=self.parse_data, dont_filter=True)
 
-            yield scrapy.Request(url=url, callback=self._parse_all, dont_filter=True)
-
-    def _parse_all(self, response):
-        url_difficulty = response.url
-
-        for i in range(1, 50):
-            if "Next" in response.xpath('//div[@style="margin-bottom: 10px; margin-top: 10px;"]/a/text()').extract():
-                href = url_difficulty + "?page={id}"
-                href = href.format(id=i)
-                yield scrapy.Request(url=href, callback=self._parse_pagenation, dont_filter=True)
-            else:
-                break
-        href = url_difficulty + "?page={id}"
-        href = href.format(id=i)
-        yield scrapy.Request(url=href, callback=self._parse_pagenation, dont_filter=True)
-
-    def _parse_pagenation(self, response):
-        href_links = response.xpath('//ul[@class="grid246'
-                                    '8"]/li[@class="grid-item"]/a/@href').extract()
-        for href in href_links:
-            url = 'https://www.hooktheory.com%s' % href
-            yield scrapy.Request(url=url, callback=self._parse_data, dont_filter=True)
-
-    def _parse_data(self, response):
-        api_url = 'https://www.hooktheory.com/songs/getXmlByPk?pk={id}'
-
+    def parse_data(self, response):
         song = response.xpath('//title/text()').extract()
         song = song[0].split(' by')[0]
+
         # Get data from "Verse Tag"
         if response.xpath('//div[contains(@id, "verse")]/div/@id'):
             verse_id = response.xpath('//div[contains(@id, "verse")]/div/@id').extract()
-            api_verse = api_url.format(id=verse_id[0])
+            api_verse = self.API_URL.format(id=verse_id[0])
             data_verse = requests.get(url=api_verse).content
 
             if re.search('<notes>(.*)</notes>', data_verse, re.DOTALL):
@@ -63,11 +41,6 @@ class FlashApp(scrapy.Spider):
                 verse_chords = re.search('<chords>(.*)</chords>', data_verse, re.DOTALL).group()
             else:
                 verse_chords = ''
-
-            # if re.search('<title>(.*)</title>', data_verse, re.DOTALL):
-            #     song = re.search('<title>(.*)</title>', data_verse, re.DOTALL).group()
-            # else:
-            #     song = ''
         else:
             verse_notes = ''
             verse_chords = ''
@@ -75,7 +48,7 @@ class FlashApp(scrapy.Spider):
         # Get data from "Chorus Tag"
         if response.xpath('//div[contains(@id, "chorus")]/div/@id'):
             chorus_id = response.xpath('//div[contains(@id, "chorus")]/div/@id').extract()
-            api_chorus = api_url.format(id=chorus_id[0])
+            api_chorus = self.API_URL.format(id=chorus_id[0])
             data_chorus = requests.get(url=api_chorus).content
 
             if re.search('<notes>(.*)</notes>', data_chorus, re.DOTALL):
@@ -98,11 +71,3 @@ class FlashApp(scrapy.Spider):
             'verse_notes': verse_notes,
             'verse_chords': verse_chords,
         }
-
-        # yield {
-        #     'a': unescape(song, entities={"&lt;": "<", "&gt;": ">"}),
-        #     'chorus_notes': unescape(chorus_notes),
-        #     'chorus_chords': unescape(chorus_chords),
-        #     'verse_notes': unescape(verse_notes),
-        #     'verse_chords': unescape(verse_chords),
-        # }
