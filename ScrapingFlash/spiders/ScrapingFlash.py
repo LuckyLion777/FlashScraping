35f19ff94405f5dd7c4b57bd5912322bbfdbf361
LuckyLion <always.smile0915@gmail.com>
2017-04-24 13:46:30 -0400

    Update script to write result into files

--- ScrapingFlash/spiders/ScrapingFlash.py
+++ ScrapingFlash/spiders/ScrapingFlash.py
@@ -1,14 +1,21 @@
 import scrapy
 import re
 import requests
+import json
+import xml.etree.cElementTree as ET
+# from elementtree.SimpleXMLWriter import XMLWriter
+import sys
 import cgi
-from xml.sax.saxutils import unescape
+import os
+# from xml.sax.saxutils import unescape
+# from xml.dom.minidom import parseString
 
 class FlashApp(scrapy.Spider):
     name = "flash"
 
     allowed_domains = ['https://hooktheory.com']
     start_urls = ['https://www.hooktheory.com/theorytab/difficulties/intermediate?page=50']
+    # start_urls = ['https://www.hooktheory.com/theorytab/view/yuki-nagato/yuki-muon-madobe-nite']
 
     API_URL = 'https://www.hooktheory.com/songs/getXmlByPk?pk={id}'
 
@@ -23,51 +30,62 @@ class FlashApp(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse_data, dont_filter=True)
 
     def parse_data(self, response):
-        song = response.xpath('//title/text()').extract()
-        song = song[0].split(' by')[0]
+        i = 0
+        categories = response.xpath('//div[@id="info"]//table[@class="table table-condensed"]'
+                                    '//tbody//tr//td/text()').extract()
+        for category in categories:
+            if category == "Verse":
+                verse_index = i
+            if category == "Chorus":
+                chorus_index = i
+            i += 1
 
-        # Get data from "Verse Tag"
-        if response.xpath('//div[contains(@id, "verse")]/div/@id'):
-            verse_id = response.xpath('//div[contains(@id, "verse")]/div/@id').extract()
+        # Get All Data in Verse Tag
+        if response.xpath('//div[@id="verse"]/div/@id'):
+            verse_id = response.xpath('//div[@id="verse"]/div/@id').extract()
             api_verse = self.API_URL.format(id=verse_id[0])
-            data_verse = requests.get(url=api_verse).content
-
-            if re.search('<notes>(.*)</notes>', data_verse, re.DOTALL):
-                verse_notes = re.search('<notes>(.*)</notes>', data_verse, re.DOTALL).group()
+            verse_data = requests.get(url=api_verse).content
+            # Get Title
+            if re.search('<title>(.*)</title>', verse_data, re.DOTALL):
+                song = re.search('<title>(.*)</title>', verse_data, re.DOTALL).group(1)
             else:
-                verse_notes = ''
+                song = ''
 
-            if re.search('<chords>(.*)</chords>', data_verse, re.DOTALL):
-                verse_chords = re.search('<chords>(.*)</chords>', data_verse, re.DOTALL).group()
-            else:
-                verse_chords = ''
-        else:
-            verse_notes = ''
-            verse_chords = ''
+            # Get Category name
+            category_name = response.xpath('//div[@id="info"]//table[@class="table table-condensed"]'
+                                               '//tbody//tr//td/a/@href')[verse_index].extract()
+            category_name = re.search('difficulties/(.*)', category_name, re.DOTALL).group(1)
 
-        # Get data from "Chorus Tag"
-        if response.xpath('//div[contains(@id, "chorus")]/div/@id'):
-            chorus_id = response.xpath('//div[contains(@id, "chorus")]/div/@id').extract()
+            # Write data to file inside it's own directory
+            if not os.path.exists(category_name):
+                os.makedirs(category_name)
+
+            with open(category_name + '/' + song + ".xml", 'w+') as f:
+                f.write('<?xml version="1.0" encoding="utf-8"?>\n')
+                f.write('%s\n %s\n %s\n' % ('<Verse_data>', verse_data, '</Verse_data>'))
+
+
+        # Get All Data in Chorus Tag
+        if response.xpath('//div[@id="chorus"]/div/@id'):
+            chorus_id = response.xpath('//div[@id="chorus"]/div/@id').extract()
             api_chorus = self.API_URL.format(id=chorus_id[0])
-            data_chorus = requests.get(url=api_chorus).content
+            chorus_data = requests.get(url=api_chorus).content
 
-            if re.search('<notes>(.*)</notes>', data_chorus, re.DOTALL):
-                chorus_notes = re.search('<notes>(.*)</notes>', data_chorus, re.DOTALL).group()
+            # Get Title
+            if re.search('<title>(.*)</title>', chorus_data, re.DOTALL):
+                song = re.search('<title>(.*)</title>', chorus_data, re.DOTALL).group(1)
             else:
-                chorus_notes = ''
+                song = ''
 
-            if re.search('<chords>(.*)</chords>', data_chorus, re.DOTALL):
-                chorus_chords = re.search('<chords>(.*)</chords>', data_chorus, re.DOTALL).group()
-            else:
-                chorus_chords = ''
-        else:
-            chorus_notes = ''
-            chorus_chords = ''
+            # Get Category name
+            category_name = response.xpath('//div[@id="info"]//table[@class="table table-condensed"]'
+                                           '//tbody//tr//td/a/@href')[chorus_index].extract()
+            category_name = re.search('difficulties/(.*)', category_name, re.DOTALL).group(1)
 
-        yield {
-            'a': song,
-            'chorus_notes': chorus_notes,
-            'chorus_chords': chorus_chords,
-            'verse_notes': verse_notes,
-            'verse_chords': verse_chords,
-        }
+            # Write data to file inside it's own directory
+            if not os.path.exists(category_name):
+                os.makedirs(category_name)
+
+            with open(category_name + '/' + song + ".xml", 'w+') as f:
+                f.write('<?xml version="1.0" encoding="utf-8"?>\n')
+                f.write('%s\n %s\n %s\n' % ('<Chorus_Data>', chorus_data, '</Chorus_Data>'))
